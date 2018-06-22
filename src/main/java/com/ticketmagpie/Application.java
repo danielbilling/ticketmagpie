d3b407b3511f9cbf955aa9d6258321be782632e8
Daniel Billing <10219602+danielbilling@users.noreply.github.com>
2018-06-22 13:45:38 +0100

    Update Application.java

--- src/main/java/com/ticketmagpie/Application.java
+++ src/main/java/com/ticketmagpie/Application.java
@@ -11,5 +11,24 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+}
 
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+  }
 }
