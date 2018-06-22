03bc51c8cd1c9bb3d62e5838b86b1429460d9f11
Daniel Billing <10219602+danielbilling@users.noreply.github.com>
2018-06-22 12:14:09 +0100

    Update Application.java

--- src/main/java/com/ticketmagpie/Application.java
+++ src/main/java/com/ticketmagpie/Application.java
@@ -2,6 +2,8 @@ package com.ticketmagpie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class Application {
